a5fb3f0af64b292becba6db851445669d78fa315
i-leaflet <146383587+i-leaflet@users.noreply.github.com>
2023-09-28 23:07:10 +0300

    Add files via upload

---
+++ Задание 10.pas
@@ -0,0 +1,11 @@
+﻿program num10;
+uses GraphABC;
+begin
+  SetWindowWidth(800);
+SetWindowHeight(600); 
+SetFontColor(clRed);
+SetFontStyle(fsBoldItalicUnderline);
+SetFontSize(30);
+  TextOut(250,200,'Привет Всем!');
+  TextOut(240,280,'Дорогие друзья!');
+end.
